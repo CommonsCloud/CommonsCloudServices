a0433d724a2912a536632ad3e832dcc1ba044dcc
Joshua Powell <joshua@viableindustries.com>
2014-05-02 18:56:42 -0400

    Used HTML instad of JADE

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', engines.mustache);
 app.set('view engine', 'html');
 
 app.use(favicon());
