1bc72621608e71ddcc4941dbaa0eac4ae500dde2
Joshua Powell <joshua@viableindustries.com>
2014-05-02 19:19:00 -0400

    Used HTML instad of JADE

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -8,7 +8,6 @@ router.get('/', function(request, response) {
   var url_parts = url.parse(request.url, true);
 
   var geography_param = url_parts.query['geography'];
-  // var geojson = JSON.parse(geography_param.replace(/&quot;/g,'"'));
 
   response.render('maps.html', { __geojson__: JSON.stringify(geography_param)});
 });
