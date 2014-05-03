c1d9e654117d51ab0a9640ac17155ac639b4e655
Joshua Powell <joshua@viableindustries.com>
2014-05-03 15:03:34 -0400

    Updatd teh package file;

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -32,6 +32,36 @@ router.get('/', function(request, response) {
 
 });
 
+router.get('/pdf', function(request, response) {
+
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  var url_parts = url.parse(request.url, true);
+
+  var geography_param = url_parts.query['geography'];
+  var format = url_parts.query['format'];
+
+  var requested_map_url = 'http://services.commonscloud.org/maps/live?geography=' + geography_param;
+  var requested_map_url = 'http://test.stormwaterguide.org/#/print-your-plan';
+
+  console.log('requested_map_url', requested_map_url);
+  var command = 'phantomjs generate.js ' + JSON.stringify(requested_map_url) + ' ' + format;
+
+  console.log('command', command);
+
+  child = exec(command,
+    function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+
+      var image_url = stdout.substring(0, stdout.length -1);
+
+      response.json({'url': image_url});
+  });
+
+});
+
 /* GET home page. */
 router.get('/live', function(request, response) {
 
