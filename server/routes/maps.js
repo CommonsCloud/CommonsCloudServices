06ca7e6e47788f94639a8a6d701014f76ffa54bd
Joshua Powell <joshua@viableindustries.com>
2014-05-03 10:53:47 -0400

    Simple loop of features;

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -56,7 +56,7 @@ router.get('/', function(request, response) {
       }
   });
 
-  response.render('maps.html', { __geojson__: geography_param});
+  // response.render('maps.html', { __geojson__: geography_param});
 
 });
 
