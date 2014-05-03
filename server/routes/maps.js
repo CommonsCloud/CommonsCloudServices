d9e1fabda5b490bcdc3f1b61018193ee850da21a
Joshua Powell <joshua@viableindustries.com>
2014-05-03 09:46:40 -0400

    Simple loop of features;

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -12,4 +12,14 @@ router.get('/', function(request, response) {
   response.render('maps.html', { __geojson__: geography_param});
 });
 
+/* GET home page. */
+router.get('/live', function(request, response) {
+
+  var url_parts = url.parse(request.url, true);
+
+  var geography_param = url_parts.query['geography'];
+
+  response.render('maps.html', { __geojson__: geography_param});
+});
+
 module.exports = router;
