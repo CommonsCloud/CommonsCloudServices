ea28032ba492e42390a1819903fa54b716a017f3
Joshua Powell <joshua@viableindustries.com>
2014-05-03 10:53:12 -0400

    Simple loop of features;

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -14,26 +14,50 @@ router.get('/', function(request, response) {
 
   console.log('requested_map_url', requested_map_url);
 
-  // var spooky = new Spooky({
-  //     child: {
-  //       transport: 'http'
-  //     },
-  //     casper: {
-  //       logLevel: 'debug',
-  //       verbose: true
-  //     }
-  //   }, function (err) {
-  //       // console.log('err', err);
-  //       // console.log('We are in the Node context');
-  //       // spooky.start(requested_map_url);
-  //       // spooky.then(function () {
-  //       //     this.emit('hello', 'Hello, from ' + this.evaluate(function () {
-  //       //         return document.title;
-  //       //     }));
-  //       // });
-  //       // spooky.run();
-  //   });
-  
+  var spooky = new Spooky({
+      child: {
+        transport: 'http'
+      },
+      casper: {
+        logLevel: 'debug',
+        verbose: true
+      }
+    }, function (err) {
+        // console.log('err', err);
+        // console.log('We are in the Node context');
+        // spooky.start(requested_map_url);
+        // spooky.then(function () {
+        //     this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+        //         return document.title;
+        //     }));
+        // });
+        // spooky.run();
+    });
+
+  spooky.on('error', function (e, stack) {
+      console.error(e);
+
+      if (stack) {
+          console.log(stack);
+      }
+  });
+
+  spooky.on('console', function (line) {
+    console.log(line);
+  });
+
+  spooky.on('hello', function (greeting) {
+      console.log(greeting);
+  });
+
+  spooky.on('log', function (log) {
+      if (log.space === 'remote') {
+          console.log(log.message.replace(/ \- .*/, ''));
+      }
+  });
+
+  response.render('maps.html', { __geojson__: geography_param});
+
 });
 
 /* GET home page. */
