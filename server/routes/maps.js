74c3bfda8ad741d14d2d52ec3ef98895d1306274
Joshua Powell <joshua@viableindustries.com>
2014-05-03 10:08:50 -0400

    Simple loop of features;

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -6,9 +6,15 @@ var phantom = require('phantom');
 /* GET home page. */
 router.get('/', function(request, response) {
 
+  var url_parts = url.parse(request.url, true);
+
+  var geography_param = url_parts.query['geography'];
+
+  var requested_map_url = 'http://services.commonscloud.org/maps/live?' + geography_param
+
   phantom.create(function(ph) {
     ph.createPage(function(page) {
-      page.open(request.url, function(status) {
+      page.open(requested_map_url, function(status) {
         console.log('Opened site? %s', status);
         page.render('grr.png');
         ph.exit();
