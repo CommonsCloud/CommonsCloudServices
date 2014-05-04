f82766bbf088e46da067b6c2610d995d0e5ceafe
Joshua Powell <joshua@viableindustries.com>
2014-05-04 09:31:14 -0400

    Added error html template;

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -25,7 +25,7 @@ router.post('/maps', function(request, response) {
 /* GET home page. */
 router.post('/maps/live', function(request, response) {
 
-  console.log('request from /live', request.body);
+  console.log('request from /live', JSON.parse(request.body));
 
   var geography_param = JSON.parse(request.body);
 
